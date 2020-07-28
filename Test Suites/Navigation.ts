5ef4208d65ba77df2a7cff6487b9a81d36cb363f
adamzopcsak <adamzopcsak@gmail.com>
2020-07-28 11:40:56 +0200

    Navigate to checkbox demo page test case created Navigation test suite created

--- Test Suites/Navigation.ts
+++ Test Suites/Navigation.ts
@@ -11,4 +11,16 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>5d871cbb-9a20-4500-8e67-d6a6ed5052b6</testSuiteGuid>
+   <testCaseLink>
+      <guid>0ab897a7-4587-45d0-af7f-53a100418aba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NavigateToSimpleFormDemoPage</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b60ebfa8-7f30-48cb-9668-65803a5e70fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NavigateToCheckboxDemoPage</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>

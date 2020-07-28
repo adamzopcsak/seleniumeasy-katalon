95f2c73da1f1bedd8a6c30e18895430b4aa8353d
adamzopcsak <adamzopcsak@gmail.com>
2020-07-28 11:29:07 +0200

    Navigate to Simple Form Demo Page test case created

---
+++ Test Suites/Navigation.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Navigation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5d871cbb-9a20-4500-8e67-d6a6ed5052b6</testSuiteGuid>
+</TestSuiteEntity>

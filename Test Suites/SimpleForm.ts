e71016507445ad0d6749fe77b69a08c1f5d10004
adamzopcsak <adamzopcsak@gmail.com>
2020-07-29 13:03:44 +0200

    textInputSimpleForm | test case implemented simpleFormTestData | data file created Navigation and AllTests | test suits created

---
+++ Test Suites/SimpleForm.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SimpleForm</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>40eb8a77-3fce-4ad5-8e20-12ace421f37d</testSuiteGuid>
+   <testCaseLink>
+      <guid>0ded346c-4775-4c2e-890f-1f843759e0c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SimpleForm/textInputSimpleForm</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>7c79fad5-8c53-4160-81ee-13735649d97e</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/simpleFormTestData</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>7c79fad5-8c53-4160-81ee-13735649d97e</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>textInput</value>
+         <variableId>b6ecd08d-a711-4af0-88ce-ccae9c9b3722</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>

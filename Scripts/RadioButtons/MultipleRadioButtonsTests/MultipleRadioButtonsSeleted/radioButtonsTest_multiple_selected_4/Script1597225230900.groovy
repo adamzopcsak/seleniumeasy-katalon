16d89996eb25bb5d166311bf9a0e99c49acc262f
adamzopcsak <adamzopcsak@gmail.com>
2020-08-12 12:31:52 +0200

    radioButtonsTest_multiple_selected_4 | test case implemented

--- Scripts/RadioButtons/MultipleRadioButtonsTests/MultipleRadioButtonsSeleted/radioButtonsTest_multiple_selected_4/Script1597225230900.groovy
+++ Scripts/RadioButtons/MultipleRadioButtonsTests/MultipleRadioButtonsSeleted/radioButtonsTest_multiple_selected_4/Script1597225230900.groovy
@@ -16,3 +16,17 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('')
+
+WebUI.maximizeWindow()
+
+WebUI.navigateToUrl('https://www.seleniumeasy.com/test/basic-radiobutton-demo.html')
+
+WebUI.click(findTestObject('RadioButtonsDemoPage/female_radio_button_multiple'))
+
+WebUI.click(findTestObject('RadioButtonsDemoPage/age_group_0-5_radio_button'))
+
+WebUI.click(findTestObject('RadioButtonsDemoPage/values_button_multiple'))
+
+WebUI.closeBrowser()
+
